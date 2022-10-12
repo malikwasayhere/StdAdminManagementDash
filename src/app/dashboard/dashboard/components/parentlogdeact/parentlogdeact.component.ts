8a09ebc1b3dbe27895acac5533009288cb16b6c0
malikwasayhere <malikizhan756@gmail.com>
2022-10-12 20:51:12 +0500

    Third push
    Admin Dashboard

--- src/app/dashboard/dashboard/components/parentlogdeact/parentlogdeact.component.ts
+++ src/app/dashboard/dashboard/components/parentlogdeact/parentlogdeact.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentlogdeactComponent implements OnInit {
 
+  data = [
+    { id: 1, name: 'Rajesh', email: 'rajesh@gmail.com' },
+    { id: 2, name: 'Paresh', email: 'paresh@gmail.com' },
+    { id: 3, name: 'Naresh', email: 'naresh@gmail.com' },
+    { id: 4, name: 'Suresh', email: 'suresh@gmail.com' },
+    { id: 5, name: 'Karan', email: 'karan@gmail.com' },
+  ];
+  displayedColumns = ['id', 'name', 'email'];
+
   constructor() { }
 
   ngOnInit(): void {
